e24277b1ace13c03f73e69f71f9f03d0f7688729
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-05 07:56:58 +0700

    Initial commit

---
+++ lib/TablesCollection/Words/EN/Nouns.pm
@@ -0,0 +1,22 @@
+package TablesCollection::Words::EN::Nouns;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of Tables:: modules that contain English nouns
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<Tables>:: modules:
+
+# INSERT_MODULES_LIST /^Tables::/
+
+
+=head1 SEE ALSO
+
+L<Tables>
+
+=cut
